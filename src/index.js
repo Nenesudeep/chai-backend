d8bb2e1188975d1e76ee9e19e8304b6e9ae8ef4d
B Sudeep Sanjay Kumar <sudeep@Bs-MacBook-Air.local>
2024-01-16 13:05:28 +0530

    util discussion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 
-
+import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './db/dataBase.js'
+
+const app = express();
 
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        `Server is running on ${process.env.PORT}`
+    })
+})
+.catch((err)=>{
+    console.log("Database connection Failed Due to Error:",err)
+});
